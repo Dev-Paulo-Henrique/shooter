cf7b6af309d8f4df6d4858830f16d7e0edc31627
Paulo Henrique <jwcfree11@gmail.com>
2023-12-02 11:58:23 -0300

    First Commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1905976.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 158235589, 158235587, 158235588, 158236233 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/158236234/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/158236238/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/158236235/1/ammo.js', 'preload' : true},
+];
